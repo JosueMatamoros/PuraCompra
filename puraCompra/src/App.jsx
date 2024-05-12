63c01218b5e3ece10d9d25e606a004556ada4dac
Josué Matamoros <1002matamoros@gmail.com>
2024-05-12 12:09:02 -0600

    feat: Routing added

--- puraCompra/src/App.jsx
+++ puraCompra/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-
+import Routing from './routes'
+import { Rule } from 'postcss'
 
 function App() {
   
 
   return (
     <>
-     
+      <Routing />
     </>
   )
 }

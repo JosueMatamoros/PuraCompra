f3902b114c374f6975afb14377ab520103536e04
izackk26 <newxstorecr@gmail.com>
2024-05-12 19:11:37 +0000

    feat: main webpage

--- puraCompra/src/pages/Home.jsx
+++ puraCompra/src/pages/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-        <h2>Home</h2> 
-        <p>izacckkkkk puto </p>
+      <div className='bg-black text-orange-800'>
+        <p className='text-4xl'>Hola</p>
+      </div>
   
     </>
   )

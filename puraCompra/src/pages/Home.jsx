02c5ce07dca443080e6de9045a232b1a3bad0fc9
izackk26 <newxstorecr@gmail.com>
2024-05-13 00:32:44 +0000

    feat: Homepage

--- puraCompra/src/pages/Home.jsx
+++ puraCompra/src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import Logo from '../assets/JieShopLogoWhite.png'
+
 
 export default function Home() {
   return (
     <>
-      <div className='bg-black text-orange-800'>
-        <p className='text-4xl'>Hola</p>
+    <div className="min-h-screen flex flex-col bg-black">
+      <div className="flex flex-grow">
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center px-4 py-8">
+          <div className="text-center text-white">
+            <p className="text-xl md:text-2xl lg:text-3xl font-bold">Buy everything you want with PuraCompra</p>
+            <p className="text-lg md:text-xl">The simpliest way to get your things</p>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2  flex justify-center items-center">
+          <img src={Logo} alt="Imagen decorativa" className="w-full max-w-full h-auto  object-contain md:object-cover" />
+        </div>
       </div>
-  
+
+      <div className="flex-grow bg-white text-center p-4">
+        <p className="text-black">Welcome to PuraCompra</p>
+      </div>
+    </div>
+
     </>
   )
 }

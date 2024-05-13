ee0e297597540385daa6681cf19ebf136d85ed63
Josué Matamoros <1002matamoros@gmail.com>
2024-05-12 22:35:18 -0600

    !feat: Logo changed, posible error with changing theme

--- puraCompra/src/components/header/Header.jsx
+++ puraCompra/src/components/header/Header.jsx
@@ -4,20 +4,21 @@ import { TiShoppingCart } from "react-icons/ti";
 import { IoMdHeart } from "react-icons/io";
 import Navbar from './Navbar';
 import logo from '../../assets/JieShopLogo.png'; 
+import { DarkThemeToggle, Flowbite } from "flowbite-react";
 
 export default function Header() {
   return (
-    <header className='flex justify-between items-center mx-4 py-2'>
+    <header className='flex justify-between items-center mx-4 py-2'> 
         <div className='flex items-center gap-2'>
             <Link to='/'>
                 <img src={logo} alt="JieShop Logo" className="h-14" />
             </Link>
-            <h1 className='text-2xl font-bold'>Pura Compra</h1>
         </div>
 
         <Navbar />
 
-        <div className='flex gap-4 pt-2'>
+        <div className='flex gap-4 pt-2 items-center'>
+        <DarkThemeToggle />
         <IoMdHeart className='text-2xl' />
         <TiShoppingCart className='text-2xl' />
         </div>

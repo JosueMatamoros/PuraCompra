849e7b1391e6d4004f1bbb2c5495996bec665097
Josué Matamoros <1002matamoros@gmail.com>
2024-05-16 10:53:33 -0600

    fix: Remove sinIn and login from routes

--- puraCompra/src/routes.jsx
+++ puraCompra/src/routes.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./components/login/Login";
-import SingIn from "./components/login/SignIn";
 import Header from "./components/header/Header";
 import RegisterAccount from "./components/login/RegisterAccount";
 
@@ -10,8 +8,6 @@ const Ruting = () => (
     <Router>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/sign" element={<SingIn />} />
             <Route path="/header" element={<Header />} />
             <Route path="/register" element={<RegisterAccount />} />
         </Routes>

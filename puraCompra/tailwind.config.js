a3903c52592cf76cb3587510446f346dda682d60
Josué Matamoros <1002matamoros@gmail.com>
2024-05-16 10:52:08 -0600

    fix: tailwind.config.js

--- puraCompra/tailwind.config.js
+++ puraCompra/tailwind.config.js
@@ -1,31 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const flowbite = require("flowbite-react/tailwind");
 
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
   ],
   theme: {
-    extend: {
-      keyframes: {
-        fadein: {
-          '0%': { opacity: 0, transform: 'scale(0.95)' },
-          '100%': { opacity: 1, transform: 'scale(1)' },
-        },
-        fadeout: {
-          '0%': { opacity: 1, transform: 'scale(1)' },
-          '100%': { opacity: 0, transform: 'scale(0.95)' },
-        },
-      },
-      animation: {
-        fadein: 'fadein 0.5s ease-out',
-        fadeout: 'fadeout 0.5s ease-out'
-      }
-    },
+    extend: {},
   },
   plugins: [
     flowbite.plugin(),
   ],
-};
+ 
+}

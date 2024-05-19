ae79023c0f1d51fbcdd1fcc186b59045d0a19031
Josué Matamoros <1002matamoros@gmail.com>
2024-05-19 13:13:14 -0600

    feat: Adding 3000/assets

--- Api/main.js
+++ Api/main.js
@@ -2,6 +2,8 @@ import sequelize from "./models/index.js";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import addressesRoutes from "./routes/addressesRoutes.js";
 import usersRoutes from "./routes/usersRoutes.js";
@@ -16,25 +18,21 @@ import priceHistoryRoutes from "./routes/priceHistoryRoutes.js";
 import orderDetailsRoutes from "./routes/orderDetailsRoutes.js";
 import ProductPromotionsRoutes from "./routes/productPromotionsRoutes.js";
 
-import Promotions from "./models/promotions.js";
-import User from "./models/users.js"; 
-import Orders from "./models/orders.js";
-import ProductPromotions from "./models/productPromotions.js";
-import Reviews from "./models/reviews.js";
-import TransactionLogs from "./models/transactionLogs.js";
-import Shipments from "./models/shipments.js";
-import PriceHistory from "./models/priceHistory.js";
-import OrderDetails from "./models/orderDetails.js";
-import Products from "./models/products.js";
-import Addresses from "./models/addresses.js";
-import Sellers from "./models/sellers.js";
+// Definir __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
+
+// Servir archivos estáticos desde la carpeta 'products'
+app.use('/assets', express.static(path.join(__dirname, '../puraCompra/src/assets/products')));
+
+// Rutas de la API
 app.use('/addresses', addressesRoutes);
 app.use('/users', usersRoutes);
 app.use('/promotions', promotionsRoutes);

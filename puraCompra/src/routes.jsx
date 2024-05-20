9b26d942a48ea4319ef21af4fd846e130772cc64
Josue Matamoros <1002matamoros@gmail.com>
2024-05-20 15:37:28 -0600

    feat: New routes

--- puraCompra/src/routes.jsx
+++ puraCompra/src/routes.jsx
@@ -5,6 +5,7 @@ import Header from "./components/header/Header";
 import RegisterAccount from "./components/login/RegisterAccount";
 import Products from "./pages/Products";
 import Account from "./pages/Account";
+import Shop from "./pages/Shop";
 
 const Ruting = () => (
     <Router>
@@ -14,6 +15,7 @@ const Ruting = () => (
             <Route path="/register" element={<RegisterAccount />} />
             <Route path="/products" element={<Products />} />
             <Route path="/account" element={<Account/>} />
+            <Route path="/shop" element={<Shop />} />
         </Routes>
     </Router>
 );

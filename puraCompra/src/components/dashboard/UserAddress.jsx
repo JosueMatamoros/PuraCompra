b808164c1540a7576fa1d89689b70031d53a009c
iZack <newxstorecr@gmail.com>
2024-05-26 04:12:54 -0600

    feat: Adding method to add directions for users

--- puraCompra/src/components/dashboard/UserAddress.jsx
+++ puraCompra/src/components/dashboard/UserAddress.jsx
@@ -1,10 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
 import DirectionCard from '../card/DirectionCard';
 import { AuthContext } from '../../context/AuthContext';
+import AddAddressModal from '../modal/AddAddressModal';
+import { FaPlus } from "react-icons/fa6";
+import { Button } from 'flowbite-react';
 
 export default function UserAddress() {
   const { user, updateAddresses } = useContext(AuthContext);
   const [addresses, setAddresses] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     if (user) {
@@ -20,6 +24,20 @@ export default function UserAddress() {
     updateAddresses(updatedAddresses);
   };
 
+  const handleAddAddress = (newAddress) => {
+    const updatedAddresses = [...addresses, newAddress];
+    setAddresses(updatedAddresses);
+    updateAddresses(updatedAddresses);
+  };
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className='flex flex-col items-start'>
       <div className='flex flex-row'>
@@ -32,7 +50,17 @@ export default function UserAddress() {
             onUpdate={(newAddress) => handleUpdateAddress(newAddress, index)}
           />
         ))}
+        <div className="flex items-center justify-center mx-2 my-2">
+          <button onClick={handleOpenModal} className="inline-flex items-center justify-center w-8 h-8 text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <FaPlus className="w-4 h-4" />
+          </button>
+          </div>
+        </div>
+        <AddAddressModal
+          show={isModalOpen}
+          onClose={handleCloseModal}
+          onAddAddress={handleAddAddress}
+        />
       </div>
-    </div>
-  );
+      );
 }

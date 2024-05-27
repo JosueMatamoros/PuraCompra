80aebc3f1b93879165b445fba67444f86b427db0
iZack <newxstorecr@gmail.com>
2024-05-26 18:14:08 -0600

    feat: Updating thigs

--- puraCompra/src/components/dashboard/UserAddress.jsx
+++ puraCompra/src/components/dashboard/UserAddress.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import DirectionCard from '../card/DirectionCard';
 import { AuthContext } from '../../context/AuthContext';
 import AddAddressModal from '../modal/AddAddressModal';
-import { FaPlus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
 import { Button } from 'flowbite-react';
 
 export default function UserAddress() {
@@ -38,29 +38,44 @@ export default function UserAddress() {
     setIsModalOpen(false);
   };
 
+  // Función para dividir las direcciones en filas de tres columnas
+  const chunkArray = (array, chunkSize) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+  };
+
+  const rows = chunkArray(addresses, 3);
+
   return (
     <div className='flex flex-col items-start'>
-      <div className='flex flex-row'>
-        {addresses && addresses.map((address, index) => (
-          <DirectionCard
-            key={address.AddressID}
-            directionNumber={`Direction ${index + 1}`}
-            direction={address.address}
-            addressID={address.AddressID}
-            onUpdate={(newAddress) => handleUpdateAddress(newAddress, index)}
-          />
-        ))}
-        <div className="flex items-center justify-center mx-2 my-2">
-          <button onClick={handleOpenModal} className="inline-flex items-center justify-center w-8 h-8 text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <FaPlus className="w-4 h-4" />
-          </button>
-          </div>
+      {rows.map((row, rowIndex) => (
+        <div key={rowIndex} className='flex flex-row mb-4'>
+          {row.map((address, index) => (
+            <DirectionCard
+              key={address.AddressID}
+              directionNumber={`Direction ${index + 1 + rowIndex * 3}`}
+              direction={address.address}
+              addressID={address.AddressID}
+              onUpdate={(newAddress) => handleUpdateAddress(newAddress, index + rowIndex * 3)}
+            />
+          ))}
+          {rowIndex === rows.length - 1 && row.length < 3 && (
+            <div className="flex items-center justify-center mx-2 my-2">
+              <button onClick={handleOpenModal} className="inline-flex items-center justify-center w-8 h-8 text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <FaPlus className="w-4 h-4" />
+              </button>
+            </div>
+          )}
         </div>
-        <AddAddressModal
-          show={isModalOpen}
-          onClose={handleCloseModal}
-          onAddAddress={handleAddAddress}
-        />
-      </div>
-      );
+      ))}
+      <AddAddressModal
+        show={isModalOpen}
+        onClose={handleCloseModal}
+        onAddAddress={handleAddAddress}
+      />
+    </div>
+  );
 }

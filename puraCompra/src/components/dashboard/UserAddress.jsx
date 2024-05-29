4100c35d6f3e11b76bcec44f9703b4c8e949d77f
Perky219 <perky6929@gmail.com>
2024-05-28 22:01:00 -0600

    feat: adding delete address button

--- puraCompra/src/components/dashboard/UserAddress.jsx
+++ puraCompra/src/components/dashboard/UserAddress.jsx
@@ -3,7 +3,6 @@ import DirectionCard from '../card/DirectionCard';
 import { AuthContext } from '../../context/AuthContext';
 import AddAddressModal from '../modal/AddAddressModal';
 import { FaPlus } from "react-icons/fa";
-import { Button } from 'flowbite-react';
 
 export default function UserAddress() {
   const { user, updateAddresses } = useContext(AuthContext);
@@ -26,6 +25,12 @@ export default function UserAddress() {
 
   const handleAddAddress = (newAddress) => {
     const updatedAddresses = [...addresses, newAddress];
+    setAddresses(updatedAddresses);
+    updateAddresses(updatedAddresses);
+  };
+
+  const handleDeleteAddress = (addressID) => {
+    const updatedAddresses = addresses.filter(address => address.AddressID !== addressID);
     setAddresses(updatedAddresses);
     updateAddresses(updatedAddresses);
   };
@@ -60,6 +65,7 @@ export default function UserAddress() {
               direction={address.address}
               addressID={address.AddressID}
               onUpdate={(newAddress) => handleUpdateAddress(newAddress, index + rowIndex * 3)}
+              onDelete={handleDeleteAddress}
             />
           ))}
           {rowIndex === rows.length - 1 && row.length < 3 && (
@@ -71,6 +77,13 @@ export default function UserAddress() {
           )}
         </div>
       ))}
+      {addresses.length === 0 && (
+        <div className="flex items-center justify-center mx-2 my-2">
+          <button onClick={handleOpenModal} className="inline-flex items-center justify-center w-8 h-8 text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <FaPlus className="w-4 h-4" />
+          </button>
+        </div>
+      )}
       <AddAddressModal
         show={isModalOpen}
         onClose={handleCloseModal}

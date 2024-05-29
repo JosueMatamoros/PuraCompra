f5ff9e8ac0c1f95f16cec27765baa6e6d84b982a
Perky219 <perky6929@gmail.com>
2024-05-28 22:37:18 -0600

    feat: Adding a modal for address deletion

--- puraCompra/src/components/Card/DirectionCard.jsx
+++ puraCompra/src/components/Card/DirectionCard.jsx
@@ -10,6 +10,7 @@ export default function DirectionCard({ directionNumber, direction, addressID, o
     const [newAddress, setNewAddress] = useState(direction);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [showToast, setShowToast] = useState(false);
+    const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
 
     const handleOpenModal = () => {
         setIsModalOpen(true);
@@ -17,6 +18,14 @@ export default function DirectionCard({ directionNumber, direction, addressID, o
 
     const handleCloseModal = () => {
         setIsModalOpen(false);
+    };
+
+    const handleOpenConfirmModal = () => {
+        setIsConfirmModalOpen(true);
+    };
+
+    const handleCloseConfirmModal = () => {
+        setIsConfirmModalOpen(false);
     };
 
     const handleSave = async () => {
@@ -47,6 +56,7 @@ export default function DirectionCard({ directionNumber, direction, addressID, o
             const response = await axios.delete(`http://localhost:3000/addresses/${addressID}`);
             if (response.status === 200) {
                 onDelete(addressID);
+                handleCloseConfirmModal();
             } else {
                 console.error('Failed to delete address');
             }
@@ -72,7 +82,7 @@ export default function DirectionCard({ directionNumber, direction, addressID, o
                 <button onClick={handleOpenModal} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Edit
                 </button>
-                <button onClick={handleDelete} className="inline-flex items-center px-3 py-2 ml-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                <button onClick={handleOpenConfirmModal} className="inline-flex items-center px-3 py-2 ml-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                     Delete
                 </button>
             </div>
@@ -98,6 +108,20 @@ export default function DirectionCard({ directionNumber, direction, addressID, o
                 <Modal.Footer>
                     <Button onClick={handleCloseModal}>Cancel</Button>
                     <Button variant="primary" onClick={handleSave}>Save</Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={isConfirmModalOpen} onClose={handleCloseConfirmModal}>
+                <Modal.Header>
+                    Confirm Deletion
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="space-y-6">
+                        <p>Are you sure you want to delete this address?</p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button onClick={handleCloseConfirmModal} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black rounded-lg border border-gray-400 hover:border-blue-300">Cancel</button>
+                    <button onClick={handleDelete} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete</button>
                 </Modal.Footer>
             </Modal>
             {showToast && (
--- puraCompra/src/components/dashboard/UserAddress.jsx
+++ puraCompra/src/components/dashboard/UserAddress.jsx
@@ -3,7 +3,6 @@ import DirectionCard from "../card/DirectionCard";
 import { AuthContext } from "../../context/AuthContext";
 import AddAddressModal from "../modal/AddAddressModal";
 import { FaPlus } from "react-icons/fa";
-import { Button } from "flowbite-react";
 
 export default function UserAddress() {
   const { user, updateAddresses } = useContext(AuthContext);
@@ -85,13 +84,18 @@ export default function UserAddress() {
         </div>
       ))}
       {addresses.length === 0 && (
-        <div className="flex items-center justify-center w-full h-full border rounded-lg border-gray-300 mx-2 my-2">
-          <button
-            onClick={handleOpenModal}
-            className="inline-flex items-center justify-center w-12 h-12 text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <div className="flex flex-row mb-4">
+          <div
+            className="flex items-center justify-center w-full h-full border rounded-lg border-gray-300 mx-2 my-2"
+            style={{ width: "18rem", height: "27rem" }}
           >
-            <FaPlus className="w-6 h-6" />
-          </button>
+            <button
+              onClick={handleOpenModal}
+              className="inline-flex items-center justify-center w-12 h-12 text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              <FaPlus className="w-6 h-6" />
+            </button>
+          </div>
         </div>
       )}
       <AddAddressModal
